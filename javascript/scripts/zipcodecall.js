a0d1a10ffbd83c9f85f28982b7b5fd9adcc64e2e
NavidG23 <navd.galt@gmail.com>
2018-04-10 23:43:45 -0400

    jsonp

--- javascript/scripts/zipcodecall.js
+++ javascript/scripts/zipcodecall.js
@@ -11,7 +11,7 @@ function validateZip()
 //        url : "https://api.jsonbin.io/b/5aabf8cdaba566611f330081",
         url : "https://navidg23.github.io/SWE642Website/zipcodes.json",
         type : "GET",
-        dataType:"json",
+        dataType:"jsonp",
         success : function(data) 
         { 
             if (zipRegEx.test(currentZipcode) == false) 
